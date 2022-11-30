3a23765b3afde4cd7f582a3a30a495677bf240a8
Don Weeks <donaldweeksiii@gmail.com>
2022-11-29 22:41:06 -0500

    init

---
+++ ytdownloader.py
@@ -0,0 +1,70 @@
+
+from tkinter import *
+from tkinter import filedialog
+from moviepy import *
+from moviepy.editor import VideoFileClip
+from moviepy.editor import AudioFileClip
+from pytube import YouTube
+
+import shutil
+
+
+#functions
+def select_path():
+    # allows user to select path
+    path = filedialog.askdirectory()
+    path_label.config(text=path)
+
+def download_file():
+    #get user path
+    get_link = link_field.get()
+    #get selected path
+    user_path = path_label.cget('text')
+    screen.title('Downloading...')
+    #download video
+    mp4_video = YouTube(get_link).streams.get_highest_resolution().download()
+    vid_clip = VideoFileClip(mp4_video)
+    vid_clip.close()
+    #move file to selected directory
+    shutil.move(mp4_video, user_path)
+    screen.title('Download Complete! Download Another File...')
+
+screen = Tk()
+
+title = screen.title("Don's YouTube Downloader")
+canvas = Canvas(screen, width=500, height=500)
+canvas.pack()
+
+#image
+logo_img = PhotoImage(file='yt.png')
+#resize
+logo_img = logo_img.subsample(2, 2)
+canvas.create_image(250, 80, image=logo_img)
+
+#link field
+link_field = Entry(screen, width=50)
+link_label = Label(screen, text='Enter Download Link: ', font=('Arial, 15'))
+
+#Select path
+path_label = Label(screen, text='Select Path For Download', font=('Arial, 15'))
+select_btn = Button(screen, text='Select', command=select_path)
+
+
+
+# add to window
+canvas.create_window(250, 280, window=path_label)
+canvas.create_window(250, 330, window=select_btn)
+
+
+# add Widgets to window
+canvas.create_window(250, 170, window=link_label)
+canvas.create_window(250, 220, window=link_field)
+
+#download buttons
+download_btn = Button(screen, text="Download File", command=download_file)
+# add to canvas
+canvas.create_window(250, 390, window=download_btn)
+
+
+
+screen.mainloop()
